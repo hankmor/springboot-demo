864d0f3b871f76fe7c9cfbb734329da87e879056
sun <sunfuchang03@126.com>
2018-06-22 00:12:34 +0800

    添加jpa部分demo

--- 03-spring-data-jpa/src/main/java/com/belonk/dao/EmployeeDao.java
+++ 03-spring-data-jpa/src/main/java/com/belonk/dao/EmployeeDao.java
@@ -2,14 +2,39 @@ package com.belonk.dao;
 
 import com.belonk.entity.Department;
 import com.belonk.entity.Employee;
+import com.belonk.entity.Gender;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.scheduling.annotation.Async;
 
 import java.util.List;
+import java.util.concurrent.Future;
+import java.util.stream.Stream;
 
 public interface EmployeeDao extends BaseDao<Employee> {
     List<Employee> findByNameLike(String name);
 
     @Query("select d from Employee e, Department d where e.departmentId = d.id and e.id = :employeeId")
     Department findDepartmentById(@Param("employeeId") Long employeeId);
+
+    List<Employee> findTop3ByNameLikeOrderByIdAsc(String name);
+
+    Employee findTopByNameLikeOrderByIdAsc(String name);
+
+    Employee findFirstByNameLikeOrderByIdAsc(String name);
+
+    List<Employee> findFirst3ByNameLikeOrderByIdAsc(String name);
+
+    Page<Employee> findByNameLikeOrderByIdAsc(String name, Pageable pageable);
+
+    Slice<Employee> findByAgeGreaterThanEqualOrderByIdAsc(int age, Pageable pageable);
+
+    @Query("select e from Employee e where e.name like ?1")
+    Stream<Employee> findByCustomQueryAndStream(String name);
+
+    @Async
+    Future<List<Employee>> findByGender(Gender gender);
 }
--- 03-spring-data-jpa/src/main/resources/data.sql
+++ 03-spring-data-jpa/src/main/resources/data.sql
@@ -1,7 +1,13 @@
 INSERT INTO `spring-boot`.`employee` (`id`, `age`, `gender`, `name`, `departmentId`) VALUES ('1', '10', b'1', 'ella', '1');
 INSERT INTO `spring-boot`.`employee` (`id`, `age`, `gender`, `name`, `departmentId`) VALUES ('2', '12', b'0', 'hebe', '1');
-INSERT INTO `spring-boot`.`employee` (`id`, `age`, `gender`, `name`, `departmentId`) VALUES ('3', '22', b'1', 'alice', '2');
-INSERT INTO `spring-boot`.`employee` (`id`, `age`, `gender`, `name`, `departmentId`) VALUES ('4', '20', b'0', 'lyli', '2');
+INSERT INTO `spring-boot`.`employee` (`id`, `age`, `gender`, `name`, `departmentId`) VALUES ('3', '14', b'1', 'alice', '2');
+INSERT INTO `spring-boot`.`employee` (`id`, `age`, `gender`, `name`, `departmentId`) VALUES ('4', '15', b'0', 'lyli', '2');
+INSERT INTO `spring-boot`.`employee` (`id`, `age`, `gender`, `name`, `departmentId`) VALUES ('5', '21', b'1', 'test1', '1');
+INSERT INTO `spring-boot`.`employee` (`id`, `age`, `gender`, `name`, `departmentId`) VALUES ('6', '22', b'1', 'test2', '1');
+INSERT INTO `spring-boot`.`employee` (`id`, `age`, `gender`, `name`, `departmentId`) VALUES ('7', '23', b'1', 'test3', '1');
+INSERT INTO `spring-boot`.`employee` (`id`, `age`, `gender`, `name`, `departmentId`) VALUES ('8', '24', b'1', 'test4', '1');
+INSERT INTO `spring-boot`.`employee` (`id`, `age`, `gender`, `name`, `departmentId`) VALUES ('9', '25', b'1', 'test5', '1');
+INSERT INTO `spring-boot`.`employee` (`id`, `age`, `gender`, `name`, `departmentId`) VALUES ('10', '26', b'1', 'test6', '1');
 
 INSERT INTO `spring-boot`.`department` (`id`, `name`, `leaderId`) VALUES ('1', '技术部', '1');
 INSERT INTO `spring-boot`.`department` (`id`, `name`, `leaderId`) VALUES ('2', '市场部', '2');
