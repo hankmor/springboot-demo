76b08d01b3c3b4a7c7e44a190fd807766a7ecac9
sun <belonk.sun@abcbooking.cn>
2019-06-17 20:06:24 +0800

    add spring amqp message converter demo

--- 12-amqp/00-spring-amqp-annotation/src/main/java/com/belonk/SpringAmqpApplication.java
+++ 12-amqp/00-spring-amqp-annotation/src/main/java/com/belonk/SpringAmqpApplication.java
@@ -1,22 +1,26 @@
 package com.belonk;
 
 import com.belonk.anno.*;
+import com.belonk.domain.Dept;
 import com.belonk.domain.User;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
 @SpringBootApplication
 @EnableRabbit
 public class SpringAmqpApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedEncodingException {
         ApplicationContext context = SpringApplication.run(SpringAmqpApplication.class, args);
 
-        /*// demo1
+
+        /*
+        // demo1
         HelloWorldDemo helloWorldDemo = context.getBean(HelloWorldDemo.class);
         helloWorldDemo.send("hello world!");
         helloWorldDemo.send("hi, belonk!");
@@ -33,9 +37,17 @@ public class SpringAmqpApplication {
         multiHandlerDemo.runDemo(new ArrayList<>());
         multiHandlerDemo.runDemo(new User("李四"));
         // 错误，无法（反）序列化，必须实现Serializable接口
-        // multiHandlerDemo.runDemo(new Object());*/
+        // multiHandlerDemo.runDemo(new Object());
         // demo5
         ConfirmAndReturnDemo confirmAndReturnDemo = context.getBean(ConfirmAndReturnDemo.class);
         confirmAndReturnDemo.send(new User("王五"));
+        */
+
+        // demo6，运行时需要打开RabbitConfiguration的RabbitListenerContainerFactory，使其为消息监听设置JSON转换器
+        JsonMessageDemo jsonMessageDemo = context.getBean(JsonMessageDemo.class);
+        User user = new User("赵六");
+        jsonMessageDemo.send(user);
+        jsonMessageDemo.send(new Dept("技术部", user));
+        jsonMessageDemo.send("this is not json");
     }
 }
