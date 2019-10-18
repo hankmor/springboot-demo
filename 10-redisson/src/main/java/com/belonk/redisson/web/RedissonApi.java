afaa35a63bb2c29a99ce26f5a3b0d174b83db072
sun <belonk.sun@abcbooking.cn>
2019-10-18 10:46:42 +0800

    add redisson demo code

--- 10-redisson/src/main/java/com/belonk/redisson/web/RedissonApi.java
+++ 10-redisson/src/main/java/com/belonk/redisson/web/RedissonApi.java
@@ -1,11 +1,14 @@
 package com.belonk.redisson.web;
 
+import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Random;
 
 /**
  * Created by sun on 2018/12/5.
@@ -37,6 +40,7 @@ public class RedissonApi {
 
     @Autowired
     RedissonClient client;
+    private Random random = new Random();
 
     /*
      * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -56,7 +60,65 @@ public class RedissonApi {
      * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
      */
 
+    @RequestMapping("/lock")
+    public void lock1() {
+        String lockName = "lockDemo";
+        int time1 = random.nextInt(30);
+        int time2 = random.nextInt(30);
+        int time3 = time1 + time2 + 1;
+        new Thread(() -> {
+            String thread = Thread.currentThread().getName();
+            RLock lock = client.getLock(lockName);
+            try {
+                System.err.println(thread + ": before lock ...");
+                // 锁定，使用默认的超时时间，默认是30秒
+                lock.lock();
+                System.err.println(thread + ": get lock ...");
+                System.err.println(thread + ": do something ...");
+                System.err.println(thread + ": spend " + time1 + " s");
+                Thread.sleep(time1 * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                System.err.println(thread + ": unlock ...");
+                lock.unlock();
+            }
+        }).start();
+        new Thread(() -> {
+            String thread = Thread.currentThread().getName();
+            RLock lock = client.getLock(lockName);
+            try {
+                System.err.println(thread + ": before lock ...");
+                lock.lock();
+                System.err.println(thread + ": get lock ...");
+                System.err.println(thread + ": do something ...");
+                System.err.println(thread + ": spend " + time2 + " s");
+                Thread.sleep(time2 * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                System.err.println(thread + ": unlock ...");
+                lock.unlock();
+            }
+        }).start();
+        new Thread(() -> {
+            String thread = Thread.currentThread().getName();
+            RLock lock = client.getLock(lockName);
+            System.err.println(thread + ": waiting to tryLock ...");
+            try {
+                Thread.sleep(time3 * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            // 尝试获得锁，如果锁空闲，则立即返回true，否则理解返回false
+            if (lock.tryLock()) {
+                System.err.println(thread + ": got lock successfully");
+            } else {
+                System.err.println(thread + ": got lock failed");
+            }
 
+        }).start();
+    }
 
     /*
      * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
