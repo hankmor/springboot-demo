52928aff967c667e975c4510995da5e44e6cb2d7
sun <belonk.sun@abcbooking.cn>
2019-10-30 21:46:44 +0800

    add websocket with/without stomp demo

--- 14-websocket-stomp/src/main/java/com/belonk/web/WebSocketController.java
+++ 14-websocket-stomp/src/main/java/com/belonk/web/WebSocketController.java
@@ -1,10 +1,22 @@
 package com.belonk.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
+import java.security.Principal;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Created by sun on 2019/10/28.
@@ -14,7 +26,7 @@ import javax.servlet.http.HttpSession;
  * @since 1.0
  */
 @Controller
-@RequestMapping
+@RequestMapping("/app")
 public class WebSocketController {
     /*
      * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -24,7 +36,7 @@ public class WebSocketController {
      * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
      */
 
-
+    private static Logger log = LoggerFactory.getLogger(WebSocketController.class);
 
     /*
      * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -34,7 +46,9 @@ public class WebSocketController {
      * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
      */
 
-
+    @Resource
+    private SimpMessagingTemplate simpMessagingTemplate;
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     /*
      * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -54,16 +68,51 @@ public class WebSocketController {
      * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
      */
 
-    @RequestMapping
-    public String home() {
-        return "index";
-    }
-
     @RequestMapping("/username")
     @ResponseBody
     public void setName(HttpSession session, String userName) {
-          session.setAttribute("userName", userName);
-    };
+        session.setAttribute("userName", userName);
+    }
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String customSendMsg(String name) {
+        simpMessagingTemplate.convertAndSend("/topic/chat", "hello, i am " + name);
+        return "success";
+    }
+
+    /**
+     * 发送群聊消息
+     *
+     * @param content
+     * @return
+     */
+    // 路由消息，可用在类和方法上，用在类上表示控制器中所有方法之间共享映射。格式为/foo*，/foo/**和/foo/{id}(使用@DestinationVariable来引用传递的参数)
+    @MessageMapping("/say")
+    // 将消息的返回值直接发送到指定的目标地址上
+    @SendTo("/topic/chat")
+    public String sayAll(String content, Principal principal) {
+        String curUserName = principal.getName();
+        return formatter.format(LocalDateTime.now()) + ", " + curUserName + " say : " + content;
+    }
+
+    @MessageMapping("/say/{userName}")
+    // @SendTo("/queue/{principal}/chat")
+    // @SendToUser("/queue/chat")
+    public String send2User(String content, @DestinationVariable String userName, Principal principal) {
+        System.out.println("userName : " + userName);
+        String curUserName = principal.getName();
+        // return formatter.format(LocalDateTime.now()) + ", " + curUserName + " say : " + content;
+        String s = formatter.format(LocalDateTime.now()) + ", " + curUserName + " say : " + content;
+        simpMessagingTemplate.convertAndSendToUser(userName, "/queue/" + principal + "/chat", s);
+        return s;
+    }
+
+    @MessageExceptionHandler
+    @SendTo("/queue/errors")
+    public String handleException(Throwable exception) {
+        return exception.getMessage();
+    }
 
     /*
      * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
